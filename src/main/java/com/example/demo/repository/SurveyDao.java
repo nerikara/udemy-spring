be71019433acbbf698f559b95fb87e3f8201762b
nerikara <nerikara4chimi@gmail.com>
2019-12-10 17:19:10 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/repository/SurveyDao.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.entity.Survey;
+
+public interface SurveyDao {
+
+	void insertSurvey(Survey survey);
+
+	List<Survey> getAll();
+
+}
